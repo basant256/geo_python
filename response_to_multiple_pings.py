cb5288ba1a1ec29fcb3f3cd43b92b98728f2bf32
basant256 <32891309+basant256@users.noreply.github.com>
2025-07-09 21:17:04 +0530

    Update response_to_multiple_pings.py

--- response_to_multiple_pings.py
+++ response_to_multiple_pings.py
@@ -1,1 +1,25 @@
+import socket  # noqa: F401
 
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    connection, _ = server_socket.accept()
+    # Uncomment this to pass the first stage
+    while True:
+        # wait for client
+        request: bytes = connection.recv(512)
+        data: str = request.decode()
+        # print(f"Received data: {data!r}")
+        if not data:
+            break
+        # Respond to PING command
+        if "ping" in data.lower():
+            connection.send("+PONG\r\n".encode())
+        # connection.close()
+
+
+if __name__ == "__main__":
+    main()
